e8a5569f6c198dc72e273a6b18b2e2cfc7ed91fc
Dominic V Rendero <dominic.rendero@uconn.edu>
2024-06-05 23:18:21 -0400

    Ensured hit is registered when correct word is typed

--- display_word.gd
+++ display_word.gd
@@ -3,7 +3,10 @@ extends Label
 var csv_file_path = "res://random_words_test.csv"
 var words = []
 
-@onready var word_display =  self
+@onready var display =  self
+@onready var input = get_node("/root/Node2D/Player1/input_line")
+@onready var marker = get_node("/root/Node2D/Player1/hit_marker")
+@onready var timer = get_node("/root/Node2D/Player1/hit_marker/flash_timer")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
@@ -13,11 +16,22 @@ func _ready():
 func display_word():
 	var random_index = randi() % words.size()
 	var random_word = words[random_index]
-	word_display.text = random_word
+	display.text = random_word
 	
 func _input(_event):
 	if Input.is_action_pressed("ui_accept"):
+		flash()
 		display_word()
+		
+func _on_flash_timer_timeout():
+	marker.visible = false
+	pass # Replace with function body.
+
+func flash():
+	if (display.text) == (input.text):
+		marker.visible = true
+		timer.start()
+		input.text = ""
 		
 func read_csv_into_list(path):
 	var file = FileAccess.open(path, FileAccess.READ)
