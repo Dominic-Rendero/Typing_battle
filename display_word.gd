b313d05acdc9a368dfea89e82f0bcd47487be68c
Dominic V Rendero <dominic.rendero@uconn.edu>
2024-06-11 19:04:40 -0400

    Pre-Transfer Commit

--- display_word.gd
+++ display_word.gd
@@ -20,18 +20,16 @@ func display_word():
 	
 func _input(_event):
 	if Input.is_action_pressed("ui_accept"):
-		flash()
-		display_word()
+		if (display.text) == (input.text):
+			marker.visible = true
+			timer.start()
+			display_word()
+		input.text = ""
 		
 func _on_flash_timer_timeout():
 	marker.visible = false
 	pass # Replace with function body.
 
-func flash():
-	if (display.text) == (input.text):
-		marker.visible = true
-		timer.start()
-		input.text = ""
 		
 func read_csv_into_list(path):
 	var file = FileAccess.open(path, FileAccess.READ)
