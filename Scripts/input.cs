7ed3d922fa7eddfb5d1c940e7553afd57e5b8836
Dominic V Rendero <dominic.rendero@uconn.edu>
2024-06-14 15:51:37 -0400

    Make sentences and produce fireball.

--- Scripts/input.cs
+++ Scripts/input.cs
@@ -3,22 +3,30 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
-public partial class display_word : Label{
+public partial class input : LineEdit{
     private List<string> words = new List<string>();
+    private List<string> sentences = new List<string>();
     private Label wordlabel;
-    private LineEdit input;
+    private LineEdit inputbox;
     private Line2D hitmarker;
     private Timer hittimer;
+    private Label sentencelabel;
+    private Sprite2D bighit;
     
     public override void _Ready(){
         GD.Print("start ready");
         wordlabel = GetNode<Label>("/root/Node2D/char1/display_word");
-        input = GetNode<LineEdit>("/root/Node2D/char1/input");
+        sentencelabel =  GetNode<Label>("/root/Node2D/char1/display_sentence");
+        inputbox = GetNode<LineEdit>("/root/Node2D/char1/input");
         hitmarker = GetNode<Line2D>("/root/Node2D/char1/hitmarker");
         hittimer = GetNode<Timer>("/root/Node2D/char1/hitmarker/timer");
         
+        bighit = GetNode<Sprite2D>("/root/Node2D/char1/fireball");
+        
         words = utils.csv_to_list("res://rand_words.csv");
+        sentences = utils.csv_to_list("res://rand_sentences.csv");
         GetRandomWord();
+        GetRandomSentence();
     }
     private void GetRandomWord(){
         GD.Randomize();
@@ -27,14 +35,26 @@ public partial class display_word : Label{
         var randword = words[i];
         wordlabel.Text = randword;
     } 
+    private void GetRandomSentence(){
+        GD.Randomize();
+        Random rand = new Random();
+        int i = rand.Next(sentences.Count);
+        var randsen = sentences[i];
+        sentencelabel.Text = randsen;
+    } 
+    
     public override void _Input(InputEvent @event){
         if (@event.IsActionPressed("ui_accept")){
-            if(wordlabel.Text == input.Text){
+            if(wordlabel.Text == inputbox.Text){
                 GetRandomWord();
                 hitmarker.Visible = true;
                 hittimer.Start();
             }
-            input.Text = "";
+            if(sentencelabel.Text == inputbox.Text){
+                GetRandomSentence();
+                bighit.Visible = true;
+            }
+            inputbox.Text = "";
         }
     }
 }
